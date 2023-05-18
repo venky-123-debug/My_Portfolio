4d5e7b6b72da691b4677b81e89f805f865a2b603
venkatesh c <venkat@print2block.com>
2023-05-19 00:05:46 +0530

    navbar completed

---
+++ src/Components/navBar.svelte
@@ -0,0 +1,21 @@
+<div class="flex items-center bg-black w-full py-6 px-6 justify-between">
+  <div class="flex items-center justify-center gap-3 text-lg">
+    <i class="fa-duotone fa-chart-simple text-white" />
+    <div class="text-blue-500 font-bold text-lg">
+      Venkatesh <span class="text-white text-base">.</span>
+      C
+    </div>
+  </div>
+
+  <div class="flex items-center justify-center gap-6">
+    <div class="flex items-cente justify-center">
+      <button class="text-orange-400 font-semibold text-base">Home</button>
+    </div>
+    <div class="flex items-center justify-end ml-auto">
+      <button class="text-orange-400 font-semibold text-base">Profile</button>
+    </div>
+    <div class="flex items-center justify-end ml-auto">
+      <button class="text-orange-400 font-semibold text-base">Skills</button>
+    </div>
+  </div>
+</div>
---
+++ src/Components/page_1.svelte
@@ -0,0 +1,3 @@
+<div class="flex w-screen h-screen bg-black">
+
+</div>
