aed63e1e94f66386e2aca9d5a3f3f93b1b7a422e
venkatesh c <venkat@print2block.com>
2023-11-18 22:53:28 +0530

    code has been moved to pages folder

---
+++ src/pages/navbar/sitemap.js/sitemap.js
@@ -0,0 +1,42 @@
+module.exports = {
+  home: [
+    {
+      id: "publish-single",
+      title: "home",
+      iconClasses: "fa-solid fa-file",
+      route: "#/home",
+    }
+  ],
+  about: [
+    {
+      id: "documents-single",
+      title: "about",
+      iconClasses: "fa-solid fa-file",
+      route: "#/about",
+    }
+  ],
+  resume: [
+    {
+      id: "resume",
+      title: "resume",
+      iconClasses: "fa-solid fa-file",
+      route: "#/resume",
+    }
+  ],
+  portfolio: [
+    {
+      id: "portfolio",
+      title: "portfolio",
+      iconClasses: "fa-solid fa-file",
+      route: "#/templates/document",
+    }
+  ],
+  testimonial: [
+    {
+      id: "testimonial",
+      title: "testimonial",
+      iconClasses: "fa-solid fa-house",
+      route: "#/settings/integration/home",
+    }
+  ],
+}
