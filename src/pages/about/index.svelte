0da7131a603d2934e7edbf91daba7c55eb4dca7b
venkatesh c <venkat@print2block.com>
2023-11-19 02:51:47 +0530

    about page tets placed in one on one

---
+++ src/pages/about/index.svelte
@@ -0,0 +1,11 @@
+<div class="flex justify-center m-auto bg-aboutBg w-screen min-h-screen overflow-hidden">
+  <div class="relative top-6 mx-auto h-32">
+    <div class="relative text-center">
+      <span class="text-8xl opacity-10 text-gray-500 font-bold relative z-10">ABOUT ME</span>
+      <div class="absolute w-3/4 mx-auto text-white text-4xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        Know Me More
+        <div class="border mt-3 border-green-300 border-b-2 w-1/4 mx-auto" />
+      </div>
+    </div>
+  </div>
+</div>
