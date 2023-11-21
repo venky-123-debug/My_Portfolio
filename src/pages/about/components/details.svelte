23d8c3cb8801a289b9f39d9a1bc845f58e0b1dff
venkatesh c <venkat@print2block.com>
2023-11-21 23:08:01 +0530

    about page has been completed

---
+++ src/pages/about/components/details.svelte
@@ -0,0 +1,59 @@
+<script>
+  import html2pdf from 'html2pdf.js';
+
+  const downloadCV = () => {
+    // const cvContent = `
+    //   <div class="w-full flex-col flex gap-3 divide-y divide-gray-500">
+    //     <div class="flex items-center gap-3 pt-3">
+    //       <div class="text-lg font-semibold text-gray-300">NAME:</div>
+    //       <div class="text-lg font-normal text-gray-400">Venkatesh. C</div>
+    //     </div>
+    //     <div class="flex items-center gap-3 pt-3">
+    //       <div class="text-lg font-semibold text-gray-300">Email:</div>
+    //       <div class="text-lg font-normal text-green-400">venkatbe26494@gmail.com</div>
+    //     </div>
+    //     <div class="flex items-center gap-3 pt-3">
+    //       <div class="text-lg font-semibold text-gray-300">Role:</div>
+    //       <div class="text-lg font-normal text-gray-400">Web Developer</div>
+    //     </div>
+    //     <div class="flex items-center gap-3 pt-3">
+    //       <div class="text-lg font-semibold text-gray-300">From:</div>
+    //       <div class="text-lg font-normal text-gray-400">Chennai, TamilNadu</div>
+    //     </div>
+    //   </div>
+    // `;
+
+    // const element = document.createElement('div');
+    // element.innerHTML = cvContent;
+
+    // html2pdf(element, {
+    //   margin: 10,
+    //   filename: 'MyCV.pdf',
+    //   image: { type: 'jpeg', quality: 0.98 },
+    //   html2canvas: { scale: 2 },
+    //   jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+    // });
+  };
+</script>
+
+<div class="w-full flex-col flex gap-3 divide-y divide-gray-500">
+  <div class="flex items-center gap-3 pt-3">
+    <div class="text-lg font-semibold text-gray-300">NAME:</div>
+    <div class="text-lg font-normal text-gray-400">Venkatesh. C</div>
+  </div>
+  <div class="flex items-center gap-3 pt-3">
+    <div class="text-lg font-semibold text-gray-300">Email:</div>
+    <div class="text-lg font-normal text-green-400">venkatbe26494@gmail.com</div>
+  </div>
+  <div class="flex items-center gap-3 pt-3">
+    <div class="text-lg font-semibold text-gray-300">Role:</div>
+    <div class="text-lg font-normal text-gray-400">Web Developer</div>
+  </div>
+  <div class="flex items-center gap-3 pt-3">
+    <div class="text-lg font-semibold text-gray-300">From:</div>
+    <div class="text-lg font-normal text-gray-400">Chennai, TamilNadu</div>
+  </div>
+  <div class="text-center pt-6">
+    <button on:click={downloadCV} type="button" class="rounded-md w-32 border border-green-300 bg-transparent py-2 text-sm font-medium text-white hover:bg-green-600 active:bg-green-500">Download CV</button>
+  </div>
+</div>
--- src/pages/about/index.svelte
+++ src/pages/about/index.svelte
@@ -1,5 +1,9 @@
+<script>
+  import Details from "./components/details.svelte"
+</script>
+
 <div class="flex flex-col m-auto bg-aboutBg w-screen min-h-screen overflow-hidden p-8">
-  <div class="relative top-6 mx-auto h-32">
+  <div class="relative top-12 mx-auto h-32">
     <div class="relative text-center">
       <span class="text-8xl opacity-10 text-gray-500 font-bold relative z-10">ABOUT ME</span>
       <div class="absolute w-3/4 mx-auto text-white text-4xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -9,18 +13,28 @@
     </div>
   </div>
 
-  <div class="flex w-full pt-12 items-center gap-3">
-    <div class="w-3/5 flex flex-col gap-3">
+  <div class="flex w-full pt-24 items-start gap-6">
+    <div class="w-4/6 flex flex-col gap-3">
       <div class="text-3xl font-bold text-white">
-        I'm <strong class="text-green-300">Venkatesh. C,</strong> a Web Developer
+        I'm <strong class="text-green-300">Venkatesh. C,</strong>
+        a Web Developer
       </div>
-      <div class="text-lg text-justify font-semibold text-gray-300">
-        Results-driven web developer with a passion for creating efficient, user-friendly, and visually appealing websites. Leveraging a strong foundation in front-end and back-end technologies, I aim to contribute innovative solutions to enhance user experiences.
-      </div>
-      <div class="text-lg text-justify font-semibold text-gray-300">
-        Seeking a challenging role where I can apply my technical skills, creativity, and dedication to deliver high-quality web applications while staying abreast of emerging trends and technologies in the ever-evolving world of web development.
-      </div>
+      <div class="text-lg text-justify font-normal text-gray-400">Results-driven web developer with a passion for creating efficient, user-friendly, and visually appealing websites. Leveraging a strong foundation in front-end and back-end technologies, I aim to contribute innovative solutions to enhance user experiences.</div>
+      <div class="text-lg text-justify font-normal text-gray-400">Seeking a challenging role where I can apply my technical skills, creativity, and dedication to deliver high-quality web applications while staying abreast of emerging trends and technologies in the ever-evolving world of web development.</div>
     </div>
-    <div class="w-2/5 bg-green-800 h-16" />
+    <div class="w-2/6">
+      <Details />
+    </div>
+  </div>
+
+  <div class="pt-8 flex items-center justify-around w-full">
+    <div class="flex flex-col w-1/2 items-center justify-center border-r border-gray-500">
+      <div class="text-5xl font-bold text-gray-400">1+</div>
+      <div class="text-xl font-semi text-gray-500">Years Experiance</div>
+    </div>
+    <div class="flex flex-col w-1/2 items-center justify-center">
+      <div class="text-5xl font-bold text-gray-400">1+</div>
+      <div class="text-xl font-semi text-gray-500">Projects Done</div>
+    </div>
   </div>
 </div>
