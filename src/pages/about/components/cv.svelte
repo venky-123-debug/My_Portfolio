4d5419d97ebb89d879ea49ff6fccb2120ec3814f
venkatesh c <venkat@print2block.com>
2023-11-21 23:12:09 +0530

    cv download feature has been added but in testing mode

--- src/pages/about/components/cv.svelte
+++ src/pages/about/components/cv.svelte
@@ -1,14 +1,4 @@
-<script>
-  import html2pdf from "html2pdf.js"
-  import Cv from "./cv.svelte"
-
-  const downloadPDF = () => {
-    const element = document.getElementById("cv")
-    html2pdf(element)
-  }
-</script>
-
-<div class="w-full flex-col flex gap-3 divide-y divide-gray-500">
+<div id="cv" class="w-full flex-col flex gap-3 divide-y divide-gray-500">
   <div class="flex items-center gap-3 pt-3">
     <div class="text-lg font-semibold text-gray-300">NAME:</div>
     <div class="text-lg font-normal text-gray-400">Venkatesh. C</div>
@@ -25,10 +15,4 @@
     <div class="text-lg font-semibold text-gray-300">From:</div>
     <div class="text-lg font-normal text-gray-400">Chennai, TamilNadu</div>
   </div>
-  <div class="text-center pt-6">
-    <button on:click={downloadPDF} type="button" class="rounded-md w-32 border border-green-300 bg-transparent py-2 text-sm font-medium text-white hover:bg-green-600 active:bg-green-500">Download CV</button>
-  </div>
-</div>
-<div class="hidden">
-  <Cv />
 </div>
