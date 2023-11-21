62e0d635d860e536a54332e03bb189566188d422
venkatesh c <venkat@print2block.com>
2023-11-21 22:19:01 +0530

    about career objective added

--- src/pages/about/index.svelte
+++ src/pages/about/index.svelte
@@ -1,4 +1,4 @@
-<div class="flex justify-center m-auto bg-aboutBg w-screen min-h-screen overflow-hidden">
+<div class="flex flex-col m-auto bg-aboutBg w-screen min-h-screen overflow-hidden p-8">
   <div class="relative top-6 mx-auto h-32">
     <div class="relative text-center">
       <span class="text-8xl opacity-10 text-gray-500 font-bold relative z-10">ABOUT ME</span>
@@ -7,5 +7,20 @@
         <div class="border mt-3 border-green-300 border-b-2 w-1/4 mx-auto" />
       </div>
     </div>
+  </div>
+
+  <div class="flex w-full pt-12 items-center gap-3">
+    <div class="w-3/5 flex flex-col gap-3">
+      <div class="text-3xl font-bold text-white">
+        I'm <strong class="text-green-300">Venkatesh. C,</strong> a Web Developer
+      </div>
+      <div class="text-lg text-justify font-semibold text-gray-300">
+        Results-driven web developer with a passion for creating efficient, user-friendly, and visually appealing websites. Leveraging a strong foundation in front-end and back-end technologies, I aim to contribute innovative solutions to enhance user experiences.
+      </div>
+      <div class="text-lg text-justify font-semibold text-gray-300">
+        Seeking a challenging role where I can apply my technical skills, creativity, and dedication to deliver high-quality web applications while staying abreast of emerging trends and technologies in the ever-evolving world of web development.
+      </div>
+    </div>
+    <div class="w-2/5 bg-green-800 h-16" />
   </div>
 </div>
