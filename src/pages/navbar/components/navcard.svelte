b18a33796641a5389877fb8dfe303746269b21ab
venkatesh c <venkat@print2block.com>
2023-11-21 23:53:49 +0530

    navbar home nav issue fixed

--- src/pages/navbar/components/navcard.svelte
+++ src/pages/navbar/components/navcard.svelte
@@ -1,13 +1,11 @@
 <script>
-  export let title=""
+  export let title = ""
   export let route
-  
-  let currentContent = location.hash
+
+  let currentContent = location.hash || "#/"
 </script>
 
-<div class="flex  group relative items-cente justify-center">
-  <div class="{currentContent ==route? "text-blue-500": "text-orange"} font-semibold text-base">{title}</div>
-  <span
-  class="absolute -bottom-1 left-0 h-[2px] w-0 {currentContent ==route? "bg-blue-500 w-full": "bg-orange group-hover:w-full"}  transition-all duration-300 "
-/>
+<div class="flex group relative items-cente justify-center">
+  <div class="{currentContent == route ? 'text-blue-500' : 'text-orange'} font-semibold text-base">{title}</div>
+  <span class="absolute -bottom-1 left-0 h-[2px] w-0 {currentContent == route ? 'bg-blue-500 w-full' : 'bg-orange group-hover:w-full'}  transition-all duration-300" />
 </div>
