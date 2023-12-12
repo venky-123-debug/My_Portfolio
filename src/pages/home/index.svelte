797cbd2f25f7af6c639da9f5ea1cfbc0d6f5ee4f
venkatesh c <venkat@print2block.com>
2023-12-12 23:20:36 +0530

    intro page created

--- src/pages/home/index.svelte
+++ src/pages/home/index.svelte
@@ -7,68 +7,22 @@
   import Services from "../whatIDo/index.svelte"
   import Summary from "../summaryPage/index.svelte"
   import Contact from "../contact/index.svelte"
+  import IntroPage from "../introPage/index.svelte"
 
-  let currentIndex = 0
-  let texts = ["I'm Venkatesh. C", "I'm a Web Developer"]
-  let currentText = texts[currentIndex]
-  let interval
-  let visibleText = false
   let aboutPage
   let currentContent = "#/"
-
-  function typewriter(node, { speed = 1, reverse = false }) {
-    const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE
-
-    if (!valid) {
-      throw new Error(`This transition only works on elements with a single text node child`)
-    }
-
-    const text = node.textContent
-    const duration = text.length / (speed * 0.01)
-
-    return {
-      duration,
-      tick: (t) => {
-        let i
-        if (reverse) {
-          i = Math.trunc(text.length * (1 - t))
-        } else {
-          i = Math.trunc(text.length * t)
-        }
-        node.textContent = text.slice(0, i)
-      },
-    }
-  }
-
-  const updateText = () => {
-    currentIndex = (currentIndex + 1) % texts.length
-    currentText = texts[currentIndex]
-  }
-
-  const toggleVisibility = () => {
-    // Toggle the visibility every 500ms
-    visibleText = !visibleText
-  }
 
   onMount(() => {
     window.addEventListener("scroll", updateCurrentContent);
     window.addEventListener("hashchange", updateCurrentContent);
     updateCurrentContent();
 
-    interval = setInterval(() => {
-      toggleVisibility()
-      updateText()
-    }, 2500) // Toggle visibility and update text every 500ms
   })
 
   onDestroy(() => {
     window.removeEventListener("scroll", updateCurrentContent);
     window.removeEventListener("hashchange", updateCurrentContent);
     clearInterval(interval)
-  })
-
-  afterUpdate(() => {
-    if (!visibleText) updateText()
   })
   function scrollToAbout() {
     aboutPage.scrollIntoView({ behavior: "smooth" })
@@ -105,25 +59,7 @@
 <div class="flex flex-col overflow-x-hidden">
   <div class="relative flex flex-col min-h-screen select-none w-screen overflow-hidden bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
     <NavBar {currentContent} />
-    <div id="home" class="flex justify-center m-auto h-full w-screen">
-      <div class="flex flex-col mx-auto gap-3">
-        <div class="text-4xl font-medium text-gray-300 text-center">Hello World</div>
-        <div class="h-[48px]">
-          {#if visibleText}
-            <div class="text-6xl text-center font-semibold text-green-300 w-full" transition:typewriter={{ speed: 1, reverse: false }}>
-              {currentText}
-            </div>
-          {:else}
-            <div class="text-6xl text-center font-semibold text-green-300 w-full">&nbsp;</div>
-          {/if}
-        </div>
-        <div class="text-3xl mt-3 font-normal text-gray-300 text-center">based in Chennai, Tamil Nadu</div>
-        <div class="text-center mt-6">
-          <button type="button" class="rounded-md w-32 border border-green-300 bg-transparent py-2 text-sm font-medium text-white hover:bg-green-600 active:bg-green-500">Hire Me</button>
-        </div>
-      </div>
-    </div>
-
+    <IntroPage />
     <div class="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-center text-gray-300">
       <!-- svelte-ignore missing-declaration -->
       <!-- svelte-ignore a11y-click-events-have-key-events -->
---
+++ src/pages/introPage/index.svelte
@@ -0,0 +1,75 @@
+<script>
+  import { afterUpdate, onDestroy, onMount } from "svelte"
+
+  let currentIndex = 0
+  let texts = ["I'm Venkatesh. C", "I'm a Web Developer"]
+  let currentText = texts[currentIndex]
+  let interval
+  let visibleText = false
+ 
+
+  onMount(() => {
+    interval = setInterval(() => {
+      toggleVisibility()
+      updateText()
+    }, 2500)
+  })
+
+  afterUpdate(() => {
+    if (!visibleText) updateText()
+  })
+  onDestroy(() => {
+    clearInterval(interval)
+  })
+  const updateText = () => {
+    currentIndex = (currentIndex + 1) % texts.length
+    currentText = texts[currentIndex]
+  }
+
+  const toggleVisibility = () => {
+    // Toggle the visibility every 500ms
+    visibleText = !visibleText
+  }
+  function typewriter(node, { speed = 1, reverse = false }) {
+    const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE
+
+    if (!valid) {
+      throw new Error(`This transition only works on elements with a single text node child`)
+    }
+
+    const text = node.textContent
+    const duration = text.length / (speed * 0.01)
+
+    return {
+      duration,
+      tick: (t) => {
+        let i
+        if (reverse) {
+          i = Math.trunc(text.length * (1 - t))
+        } else {
+          i = Math.trunc(text.length * t)
+        }
+        node.textContent = text.slice(0, i)
+      },
+    }
+  }
+</script>
+
+<div id="home" class="flex justify-center m-auto h-full w-screen">
+  <div class="flex flex-col mx-auto gap-3">
+    <div class="text-4xl font-medium text-gray-300 text-center">Hello World</div>
+    <div class="h-[48px]">
+      {#if visibleText}
+        <div class="text-6xl text-center font-semibold text-green-300 w-full" transition:typewriter={{ speed: 1, reverse: false }}>
+          {currentText}
+        </div>
+      {:else}
+        <div class="text-6xl text-center font-semibold text-green-300 w-full">&nbsp;</div>
+      {/if}
+    </div>
+    <div class="text-3xl mt-3 font-normal text-gray-300 text-center">based in Chennai, Tamil Nadu</div>
+    <div class="text-center mt-6">
+      <button type="button" class="rounded-md w-32 border border-green-300 bg-transparent py-2 text-sm font-medium text-white hover:bg-green-600 active:bg-green-500">Hire Me</button>
+    </div>
+  </div>
+</div>
